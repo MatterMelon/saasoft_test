d57f9dd0706953614011499cea03a3406dd31fe4
Andrey Popov <dil2232@gmail.com>
2025-09-21 03:58:37 +0600

    * Load data from local storage

--- src/store.ts
+++ src/store.ts
@@ -1,31 +1,12 @@
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 import type { Entry } from './types/models'
-import { LOCAL } from './types/entryTypes'
+
+const STORAGE_KEY = 'entries'
+const stored = localStorage.getItem(STORAGE_KEY)
 
 export const useEntriesStore = defineStore('entries', () => {
-  const entries = reactive<Map<string, Entry>>(new Map())
-  entries.set('1', {
-    id: '1',
-    tags: [{ text: 'Tag1' }, { text: 'Tag2' }],
-    type: LOCAL,
-    login: 'Test User 1',
-    password: '',
-  })
-  entries.set('2', {
-    id: '2',
-    tags: [{ text: 'Tag1' }, { text: 'Tag2' }],
-    type: LOCAL,
-    login: 'Test User 2',
-    password: '',
-  })
-  entries.set('3', {
-    id: '3',
-    tags: [{ text: 'Tag1' }, { text: 'Tag2' }],
-    type: LOCAL,
-    login: 'Test User 3',
-    password: '',
-  })
+  const entries = reactive<Map<string, Entry>>(new Map(stored ? JSON.parse(stored) : []))
 
   const saveEntry = (entry: Entry) => {
     entries.set(entry.id, entry)
