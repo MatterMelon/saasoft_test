79ffb52255d740161c9f234b95ff3c6a15eb0861
Andrey Popov <dil2232@gmail.com>
2025-09-21 03:23:39 +0600

    * Moved state out of component

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -1,49 +1,31 @@
 <script setup lang="ts">
 import type { Entry } from '@/types/models'
 import SingleEntry from './SingleEntry.vue'
-import { computed, reactive } from 'vue'
-import { useEntriesStore } from '@/store'
-import { Button } from 'primevue'
-import { LOCAL } from '@/types/entryTypes'
 
-const store = useEntriesStore()
-const newEntries = reactive<Map<string, Entry>>(new Map())
-const displayableEntries = computed(() => [...store.entries.values(), ...newEntries.values()])
-
-const handleDelete = (id: string) => {
-  newEntries.delete(id)
-  store.deleteEntry(id)
+interface Props {
+  entries: Entry[]
+  handleDelete: (id: string) => void
+  handleSave: (entry: Entry) => void
 }
 
-const handleSave = (entry: Entry) => {
-  newEntries.delete(entry.id)
-  store.saveEntry(entry)
-}
-
-const addNewEntry = () => {
-  const entry: Entry = {
-    id: crypto.randomUUID().toString(),
-    tags: [],
-    type: LOCAL,
-    login: '',
-    password: '',
-  }
-  newEntries.set(entry.id, entry)
-}
+const props = defineProps<Props>()
 </script>
 
 <template>
-  <Button label="" icon="pi pi-plus" type="button" class="p-button-outlined" @click="addNewEntry" />
   <div class="grid grid-cols-[1fr_0.4fr_1fr_1fr_40px] gap-2 font-bold text-gray-700">
-    <div>Метки</div>
-    <div>Тип записи</div>
+    <div class="h-10">Метки</div>
+    <div class="w-full h-10 min-w-60">Тип записи</div>
     <div>Логин</div>
     <div>Пароль</div>
     <div></div>
   </div>
   <ul class="flex flex-col gap-4">
-    <li v-for="entry in displayableEntries" :key="entry.id">
-      <SingleEntry :entry="entry" :delete-entry="handleDelete" :save-entry="handleSave" />
+    <li v-for="entry in entries" :key="entry.id">
+      <SingleEntry
+        :entry="entry"
+        :delete-entry="props.handleDelete"
+        :save-entry="props.handleSave"
+      />
     </li>
   </ul>
 </template>
