dbc31887cb6292c5e17f660d49e874d7ebc7231a
Andrey Popov <dil2232@gmail.com>
2025-09-20 18:50:00 +0600

    + Added a store for Entries

---
+++ src/store.ts
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
+import type { Entry } from './types/models'
+
+export const useEntriesStore = defineStore('entries', () => {
+  const entries = reactive<Entry[]>([
+    {
+      id: '1',
+      tags: [{ text: 'Tag1' }, { text: 'Tag2' }],
+      type: 'local',
+      login: 'Test User 1',
+      password: '',
+    },
+    {
+      id: '2',
+      tags: [{ text: 'Tag1' }, { text: 'Tag2' }],
+      type: 'local',
+      login: 'Test User 2',
+      password: '',
+    },
+    {
+      id: '3',
+      tags: [{ text: 'Tag1' }, { text: 'Tag2' }],
+      type: 'local',
+      login: 'Test User 3',
+      password: '',
+    },
+  ])
+
+  const putEntry = () => {}
+  const deleteEntry = (id: string) => {
+    entries.splice(0, entries.length, ...entries.filter((e) => e.id !== id))
+  }
+
+  return { entries, putEntry, deleteEntry }
+})
